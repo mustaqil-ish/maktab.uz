6df6c004e2d3acb5df5e56591a3a3403cff4d6f2
Sardor Haydarov <89759926+sardor0092@users.noreply.github.com>
2021-10-17 21:16:06 +0500

    uqituvchi tayyor

--- src/app/uqituvchilar/uqituvchilar.component.ts
+++ src/app/uqituvchilar/uqituvchilar.component.ts
@@ -1,15 +1,51 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-uqituvchilar',
   templateUrl: './uqituvchilar.component.html',
   styleUrls: ['./uqituvchilar.component.css']
 })
 export class UqituvchilarComponent implements OnInit {
+url="http://localhost:8080/uqituvchilar";
+uqituvchilar:any;
 
-  constructor() { }
+createForm:any;
+
+  constructor(private http:HttpClient,private formBuilder:FormBuilder ) { }
+
+
+refresh(){
+  this.http.get(this.url).subscribe(u => {
+    this.uqituvchilar =u;
+   });
+}
 
   ngOnInit(): void {
-  }
+this.refresh();
+this.createForm = this.formBuilder.group({
+  ism:[''],
+  familiya:[''],
+  yosh:[''],
+  jins:[''],
+  maosh:['']
+});
 
+
+
+   
+  }
+  saqlash(){
+    const uqituvchilar = this.createForm.value;
+    this.http.post(this.url,uqituvchilar).subscribe(data => {
+  this.refresh();
+    })
+  }
+  ochirish(id:number){
+   if(id){
+     this.http.delete(this.url+"/" + id).subscribe(date =>{
+       this.refresh();
+     })
+   }
+  }
 }
